a91213c56f35694a6e1b947c3d65194d90115878
Daniel Santana <danielmaster19@gmail.com>
2022-11-09 12:43:48 -0300

    feito edit e update

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -18,7 +18,23 @@ export class CoursesService {
     );
   }
 
-  save(record: Partial<Course>) {
-    return this.httpClient.post(this.API, record);
+  loadById(id: string) {
+    return this.httpClient.get<Course>(`${this.API}/${id}`);
   }
+
+  save(record: Partial<Course>) {
+    if (record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>){
+    return this.httpClient.post<Course>(this.API, record);
+  }
+
+  private update(record: Partial<Course>){
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record);
+  }
+
 }
